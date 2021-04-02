c29eef12ea81629f12aed50cd8c3f77ccff796dd
Andrew <andrewtayler@outlook.com.au>
2021-04-02 18:04:42 +1000

    Updated 3 pages ui css and redirection

--- main.py
+++ main.py
@@ -32,24 +32,26 @@ def file_input(self, msg):
 
     # Process file
     process_file("cache/log.cc","")
-
+    msg.page.redirect = '/log'
 
 @jp.SetRoute('/upload')
 async def homePage():
     wp = jp.WebPage()
     #wp.display_url = '/upload'
     
-    root = jp.Div(a=wp)
-
+    root = jp.Div(a=wp, classes='h-screen flex flex-col')
     navBar = NavBar(a=root)
+    border = jp.Div(classes='bg-gray-300 p-6 flex flex-col flex-1', a=root)
 
-    border = jp.Div(classes='bg-gray-300 p-6', a=root)
+    body = jp.Div(classes='bg-white grid grid-rows-2 grid-flow-col gap-8 flex flex-1 items-center justify-center h-full w-full', a=border)
+    welcomeCol = jp.Div(a=body, classes='row-span-2 m-3 p-5')
+    formCol = jp.Div(a=body, classes='row-span-2 border m-3 p-5')
+    
+    welcomeMsg = jp.H1(a=welcomeCol, text=f'Hello! Welcome to ConfigApp v0.1', classes='font-bold text-lg text-right')
+    uploadMsg = jp.P(a=welcomeCol, text='Upload a binary .CC file to begin.', classes='text-right')
+    uploadForm = UploadForm(a=formCol, submit=file_input)
 
-    body = jp.Div(classes='bg-white flex flex-col items-center justify-center h-screen', a=border)
-    welcomeMsg = jp.P(a=body, text=f'Hello! Please upload a file.', classes='font-bold')
-    uploadForm = UploadForm(a=body, submit=file_input)
-
-    copyrightMsg = jp.P(a=border, text=f'QUT Motorsport 2021', classes='text-center text-gray-600 mt-6')
+    copyrightMsg = jp.P(a=border, text=f'QUT Motorsport 2021', classes='bg-gray-300 text-center justify-center text-gray-600 pt-6')
 
     return wp
 
@@ -58,13 +60,23 @@ async def homePage():
 async def logPage():
     wp = jp.WebPage()
     
-    root = jp.Div(a=wp)
+    root = jp.Div(a=wp, classes='h-screen flex flex-col')
     navBar = NavBar(a=root)
-    border = jp.Div(classes='bg-gray-300 p-6', a=root)
+    border = jp.Div(classes='bg-gray-300 p-6 flex flex-col flex-1', a=root)
 
-    body = jp.Div(classes='flex flex-row items-center justify-center h-screen bg-white', a=border)
+    tabBar = jp.Nav(classes='flex flex-col sm:flex-row', a=border)
+    tab1 = jp.Button(a=tabBar, text='Raw Data Log', classes='text-gray-600 py-2 px-6 block hover:text-blue-500 focus:outline-none text-blue-900 border-b-2 font-medium border-blue-500')
+
+    body = jp.Div(classes='flex flex-1 bg-white justify-center', a=border)
+    
     log = pd.read_csv('export/1/rawMsgs.csv')
-    log.jp.ag_grid(a=body)  # a=wp adds the grid to WebPage wp
+    grid = jp.AgGrid(a=body, classes='', style='height: full; width: 100%; margin: 0em')
+    grid.load_pandas_frame(log)
+
+    #grid.options.columnDefs[0].cellStyle = ['width:2000']
+    #await grid.run_api('autoSizeAllColumns()', wp)
+
+    copyrightMsg = jp.P(a=border, text=f'QUT Motorsport 2021', classes='bg-gray-300 text-center justify-center text-gray-600 pt-6')
 
     return wp
 
@@ -72,28 +84,30 @@ async def logPage():
 async def analysisPage():
     wp = jp.WebPage()
     
-    root = jp.Div(a=wp)
-
+    root = jp.Div(a=wp, classes='h-screen flex flex-col')
     navBar = NavBar(a=root)
-    border = jp.Div(classes='bg-gray-300 p-6', a=root)
+    border = jp.Div(classes='bg-gray-300 p-6 flex flex-col flex-1', a=root)
 
     tabBar = jp.Nav(classes='flex flex-col sm:flex-row', a=border)
-    tab1 = jp.Button(a=tabBar, text='BMS Voltages', classes='text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none text-blue-900 border-b-2 font-medium border-blue-500')
-    tab2 = jp.Button(a=tabBar, text='Sendyne Temps', classes='text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none')
+    tab1 = jp.Button(a=tabBar, text='BMS Voltages', classes='text-gray-600 py-2 px-6 block hover:text-blue-500 focus:outline-none text-blue-900 border-b-2 font-medium border-blue-500')
+    tab2 = jp.Button(a=tabBar, text='Sendyne Temps', classes='text-gray-600 py-2 px-6 block hover:text-blue-500 focus:outline-none')
 
-    body = jp.Div(classes='flex flex-row items-center justify-center h-screen bg-white', a=border)
+    body = jp.Div(classes='flex flex-1 bg-white', a=border)
     
     bmsV = pd.read_csv('export/1/BMSvoltages_0.csv')
     voltages = bmsV.loc[:, bmsV.columns != 'timestamp'] 
 
     bms_chart = bmsV.jp.plot(0,voltages, kind='spline', a=body, title='BMS Voltage',
                subtitle='Look at this subtitle!',
-                classes='m-2 p-2 w-full h-full')
+                classes='p-2 w-full h-full')
 
     o = bms_chart.options
     o.xAxis.title.text = 'Timestamp (ms)'
     o.yAxis.title.text = 'Voltage (mV)'
     o.plotOptions.series.marker.enabled = False
+
+    copyrightMsg = jp.P(a=border, text=f'QUT Motorsport 2021', classes='bg-gray-300 text-center justify-center text-gray-600 pt-6')
+
 
     return wp
 
