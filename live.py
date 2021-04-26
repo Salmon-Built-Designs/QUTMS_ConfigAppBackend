1014d97ab67c110b76c150169b7e762fc1b4465b
Reynolds <mreynolds7785@gmail.com>
2021-04-26 15:58:41 +1000

    Need to get log_container working with new method for using panda to display

--- live.py
+++ live.py
@@ -8,6 +8,13 @@ import time
 from backend.can_parser import *
 from backend.can_ids import *
 import asyncio
+from frontend.NavBar import NavBar
+from frontend.UploadForm import UploadForm
+from frontend.Tabs import Tabs
+
+log = []
+
+wp = jp.WebPage(delete_flag=False)
 
 # Setup TCP Connection for CAN1
 TCP_IP = '192.168.0.7'
@@ -16,9 +23,6 @@ TCP_PORT_CAN2 = 20005 # Double check this
 BUFFER_SIZE = 4096
 ID_TYPE = 1
 
-wp = jp.WebPage(delete_flag=False)
-telem_div = jp.Span(text='Loading...', classes='text-5xl m-1 p-1 bg-gray-300 font-mono', a=wp)
-
 async def telem_counter():
     start = (time.time_ns() // 1_000_000 )
     while True:
@@ -26,12 +30,8 @@ async def telem_counter():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((TCP_IP, TCP_PORT_CAN1))
 
-        # s.sendall(b'\x84\x10\x50\x80\x02\x01\x02\x04\x08\x00\x00\x00\x00')
-        # #s.sendall(b'\x84\x00\x00\x06\x78\x12\x34\x56\x78\x00\x00\x00\x00')
-        # Receive any data that is available for us
         data = s.recv(BUFFER_SIZE)
-        # print(len(data))
-        # print(data)
+
         s.close()
 
         current_ms = (time.time_ns() // 1_000_000 ) - start
@@ -60,8 +60,7 @@ async def telem_counter():
         for msg in result:
             element = jp.P()
             element.text = str(msg)
-            telem_div.add(element)
-
+            log.append(element)
         
         jp.run_task(wp.update())
         await asyncio.sleep(0.1)
@@ -71,6 +70,26 @@ async def telem_init():
     jp.run_task(telem_counter())
 
 async def telem_test():
+    root = jp.Div(a=wp, classes='h-screen flex flex-col')
+    navBar = NavBar(a=root)
+    border = jp.Div(classes='bg-gray-300 p-6 flex flex-col flex-1', a=root)
+
+    if(True):
+
+        tabBar = jp.Nav(classes='flex flex-col sm:flex-row', a=border)
+        tab1 = jp.Button(a=tabBar, text='Raw Data Log', classes='text-gray-600 py-2 px-6 block hover:text-blue-500 focus:outline-none text-blue-900 border-b-2 font-medium border-blue-500')
+
+        logbody = jp.Div(classes='flex flex-1 bg-white justify-center', a=border)
+
+        global log
+        grid = jp.AgGrid(a=logbody, classes='', style='height: full; width: 100%; margin: 0em')
+        grid.load_pandas_frame(log)
+
+    else:
+        logbody = jp.Div(classes='flex flex-1 bg-white justify-center items-center', a=border)
+        jp.P(text = 'Telemetry Not Transmitting', classes='text-gray-600 font-medium', a=logbody)
+
+    copyrightMsg = jp.P(a=border, text=f'QUT Motorsport 2021', classes='bg-gray-300 text-center justify-center text-gray-600 pt-6')
     return wp
 
 jp.justpy(telem_test, host='0.0.0.0', port=80, startup=telem_init)
