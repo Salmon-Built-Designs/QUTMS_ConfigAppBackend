d876fa3d771177e68d65ede9e22feab17a308a27
Matt <mreynolds7785@gmail.com>
2021-05-06 04:58:48 +1000

    updated data reading

--- live.py
+++ live.py
@@ -23,69 +23,96 @@ BUFFER_SIZE = 4096
 ID_TYPE = 1
 
 wp = jp.WebPage(delete_flag=False)
-telem_div = jp.Span(text='Loading...', classes='text-5xl m-1 p-1 bg-gray-300 font-mono', a=wp)
+telem_div = jp.Span(text='Loading...', classes='text-5xl m-1 sock_main2-1 bg-gray-300 font-mono', a=wp)
 
 # async datainverter():
 
 async def telem_counter():
     start = (time.time_ns() // 1_000_000 )
-    file_open = open("storage/Log_" + str(start) + ".txt", 'a')
-    file_raw = open("storage/rLog_" + str(start) + ".txt", 'w+b')
-    file_datamain = open("storage/datamain" +str(start) + ".txt", 'a')
-    file_open.write(str(start))
-    # file_raw.write(start)
+
+    f_parsed = open(f"storage/{str(start)}_parsed.txt", 'a')
+    f_raw_main1 = open(f"storage/{str(start)}_raw_main1.cc", 'ab')
+    f_raw_main2 = open(f"storage/{str(start)}_raw_main2.cc", 'ab')
+    f_raw_inv1 = open(f"storage/{str(start)}_raw_inv1.cc", 'ab')
+
+
+    # parsedmain1.write(str(start))
     disp_msgs = []
 
     while True:
     #   Connect to the TCP servers
-        w = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        w.connect((TCP_IP_INV, TCP_PORT_CAN1))
+        sock_inv1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock_inv1.connect((TCP_IP_INV, TCP_PORT_CAN1))
 
-        p = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        p.connect((TCP_IP_MAIN, TCP_PORT_CAN2))
+        sock_main1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock_main1.connect((TCP_IP_MAIN, TCP_PORT_CAN1))
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((TCP_IP_MAIN, TCP_PORT_CAN1))
+        sock_main2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock_main2.connect((TCP_IP_MAIN, TCP_PORT_CAN2))
 
-        # s.send(b'\x84\x10\x50\x80\x02\x01\x02\x04\x08\x00\x00\x00\x00')
-        # s.send(b'\x84\x00\x00\x06\x78\x12\x34\x56\x78\x00\x00\x00\x00')
-        # Receive any data that is available for us
-        data = s.recv(BUFFER_SIZE)
-        datainv = w.recv(BUFFER_SIZE)
-        datamain2 = p.recv(BUFFER_SIZE)
-        # data.append(datamain)
-        # print(data)
-        file_datamain.write(str(datamain2))
-        file_raw.write(data)
-        s.close()
+        # Receive any main1data that is available for us
+        data_inv1 = sock_inv1.recv(BUFFER_SIZE)
+        data_main1 = sock_main1.recv(BUFFER_SIZE)
+        data_main2 = sock_main2.recv(BUFFER_SIZE)
+
+        sock_main1.close()
+        sock_main2.close()
+        sock_inv1.close()
 
         current_ms = (time.time_ns() // 1_000_000 ) - start
 
-        # Parse the data and unwrap key from metadata
-        raw_msgs = []
+        timestamp = (f"{str(current_ms)}\n").encode('ascii')
+
+        with open(f"storage/{str(start)}_raw_main1.cc", 'ab') as f_raw_main1:
+            f_raw_main1.write(timestamp)
+            f_raw_main1.write(data_main1)
+            f_raw_main1.write(b'\n')
+        with open(f"storage/{str(start)}_raw_main2.cc", 'ab') as f_raw_main2:
+            f_raw_main2.write(timestamp)
+            f_raw_main2.write(data_main2)
+            f_raw_main2.write(b'\n')
+        with open(f"storage/{str(start)}_raw_inv1.cc", 'ab') as f_raw_inv1:
+            f_raw_inv1.write(timestamp)
+            f_raw_inv1.write(data_inv1)
+            f_raw_inv1.write(b'\n')
+            
+        with open(f"storage/{str(start)}_raw_all.cc", 'ab') as f_raw_all:
+            f_raw_all.write(timestamp)
+            f_raw_all.write(data_main1)
+            f_raw_all.write(data_main2)
+            f_raw_all.write(data_inv1)
+            f_raw_all.write(b'\n')
+
+        data_combined = [data_inv1, data_main1, data_main2]
+
+        # Parse the main1data and unwrap key from metadata
+        channel_ID = ["CAN1", "CAN4", "CAN2"]
+        raw_msgs_all = [[],[],[]]
         idx = 0
+        for i in range(len(data_combined)):
+            raw_data = data_combined[i]
+            while True:
+                # print(f"len: {len(raw_data)} idx: {idx} remaining: {len(raw_data)-idx}")      
+                ethernetPacketInformation = raw_data[idx]
+                dataLength = (ethernetPacketInformation & 0xF)
+                # CAN ID
+                idx= idx+1
+                canId = (raw_data[idx] << 24 | raw_data[idx+1] << 16 | raw_data[idx+2] << 8 | raw_data[idx+3] << 0)
+                idx = idx + 4
+                parsedData = raw_data[idx:dataLength+idx]
 
-        while True:
-            # print(f"len: {len(data)} idx: {idx} remaining: {len(data)-idx}")      
-            ethernetPacketInformation = data[idx]
-            dataLength = (ethernetPacketInformation & 0xF)
-            # CAN ID
-            idx= idx+1
-            canId = (data[idx] << 24 | data[idx+1] << 16 | data[idx+2] << 8 | data[idx+3] << 0)
-            idx = idx + 4
-            parsedData = data[idx:dataLength+idx]
-            raw_msgs.append(raw_can_msg(current_ms, canId, ID_TYPE, dataLength, parsedData))
-            file_open.write(str(raw_can_msg(current_ms, canId, ID_TYPE, dataLength, parsedData)) + "\n")
-            idx = idx + 8
-            if (idx >= len(data)-1):
-                break
-            # Drop the messages into an array and parse them
-        # for m in raw_msgs:
-        #     print(m.id)
+                raw_msgs_all[i].append(raw_can_msg(current_ms, canId, ID_TYPE, dataLength, parsedData))
 
-        # bms_result = parse_bms_msg(raw_msgs)
+                #parsedmain1.write(str(raw_can_msg(current_ms, canId, ID_TYPE, dataLength, parsedData)) + "\n")
 
-        result = parse_can_msgs(raw_msgs, False)
+                idx = idx + 8
+                if (idx >= len(raw_data)-1):
+                    break
+
+        parsed_msgs_all = []
+        for raw_msgs in raw_msgs_all:
+            parsed_msgs_all.append(parse_can_msgs(raw_msgs, False))
+
         output = ""
 
 
@@ -103,11 +130,6 @@ async def telem_counter():
                 disp_msgs = []    
         jp.run_task(wp.update())  
         await asyncio.sleep(0.01)
-
-# async def exit_program():
-#     value = input("Choose Life or Death:\n")
-#     if (value == "Death"):
-#         sys.exit("You have chosen Death")
 
 async def telem_init():
     # x = threading.Thread(target=exit_program)
